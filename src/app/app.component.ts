6121241b28ec7f836396eb0d20c96558b5c79f1a
justinlazaro-iselect <justin.lazaro@iselect.com.au>
2020-03-02 14:36:51 +0800

    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+  FormBuilder
+} from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
+  signupForm: FormGroup;
+  forbiddenUsernames = ["Chris", "Anna"];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        userName: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this)
+        ]),
+        email: new FormControl(null, [Validators.email, Validators.required])
+      }),
+      gender: new FormControl("male"),
+      hobbies: new FormArray([])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onAddHobbdy() {
+    const control = new FormControl(null, Validators.required);
+
+    (<FormArray>this.signupForm.get("hobbies")).push(control);
+  }
+
+  getHobbiesControl() {
+    return (<FormArray>this.signupForm.get("hobbies")).controls;
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+
+    return null;
+  }
+}
